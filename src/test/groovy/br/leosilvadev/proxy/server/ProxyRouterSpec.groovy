914d6d9a99b9388817e9a1a8ff15ae0ff0986625
Leonardo <leosilvadev@gmail.com>
2016-08-11 22:45:38 -0300

    first test

---
+++ src/test/groovy/br/leosilvadev/proxy/server/ProxyRouterSpec.groovy
@@ -0,0 +1,48 @@
+package br.leosilvadev.proxy.server
+
+import io.vertx.core.Vertx
+import io.vertx.core.http.HttpMethod
+import io.vertx.ext.web.Router
+import spock.lang.Specification
+
+class ProxyRouterSpec extends Specification {
+
+	def vertx
+
+	def setup() {
+		vertx = Vertx.vertx()
+	}
+
+	def 'Should build a route'() {
+		given:
+		Router router = Router.router(vertx)
+
+		and:
+		def proxyRouter = new ProxyRouter(router)
+
+		and:
+		def method = HttpMethod.POST
+
+		and:
+		def url = 'http://myapi.io'
+
+		and:
+		def pathFrom = '/v1/users'
+
+		and:
+		def pathTo = '/users'
+
+		when:
+		def route = proxyRouter.route(method, url, pathFrom, pathTo) {}
+
+		then: 'there must be only one registered route'
+		router.routes.size() == 1
+
+		and: 'the route must be built based on pathFrom'
+		router.routes.first().path == pathFrom
+	}
+
+	def cleanup() {
+		vertx.close()
+	}
+}
