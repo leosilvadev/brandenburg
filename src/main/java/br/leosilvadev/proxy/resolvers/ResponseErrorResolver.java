402dcba0c7b8d4eebbacc917dbeaaa87dcc7833c
Leonardo <leosilvadev@gmail.com>
2016-09-29 00:51:20 +0200

    fix test

--- src/main/java/br/leosilvadev/proxy/resolvers/ResponseErrorResolver.java
+++ src/main/java/br/leosilvadev/proxy/resolvers/ResponseErrorResolver.java
@@ -4,21 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
-import org.apache.http.HttpStatus;
-
 public class ResponseErrorResolver {
 
 	private static final Map<Class<? extends Throwable>, Integer> errors;
 
 	static {
 		errors = new HashMap<>();
-		errors.put(TimeoutException.class, HttpStatus.SC_GATEWAY_TIMEOUT);
+		errors.put(TimeoutException.class, 504);
 	}
 
 	public static Integer resolveStatus(Throwable ex) {
 		for (Class<? extends Throwable> clazz : errors.keySet()) {
 			if (clazz.isInstance(ex)) return errors.get(clazz);
 		}
-		return HttpStatus.SC_INTERNAL_SERVER_ERROR;
+		return 500;
 	}
 }
