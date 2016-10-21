c69cc072d66178701a1bc236b7709183ae628912
Leonardo <leosilvadev@gmail.com>
2016-10-22 00:21:16 +0200

    new version

--- src/main/java/com/github/leosilvadev/proxy/server/EnableBrandenburgProxy.java
+++ src/main/java/com/github/leosilvadev/proxy/server/EnableBrandenburgProxy.java
@@ -6,10 +6,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import com.github.leosilvadev.proxy.config.ProxyConfigurer;
 
+@ComponentScan
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
